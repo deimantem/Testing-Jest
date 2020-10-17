980048023a862d4ea35ded0aec9107a4da4820d0
Deimante <deimante.mileryte@gmail.com>
2020-10-17 11:05:14 +0200

    jest coverage update

--- js/cloneArray.test.js
+++ js/cloneArray.test.js
@@ -3,4 +3,6 @@ const cloneArray = require('./cloneArray')
 test('Clones array properly', () => {
     const array = [1, 2, 3]
 expect(cloneArray(array)).toEqual(array)
+expect(cloneArray(array)).not.toBe(array)
+
 })
